51aef737bd5c047a244273db4c2fbd2804a54b7d
David Ripplinger <dcripplinger@gmail.com>
2017-12-04 22:55:49 -0700

    created zarahemla map

--- data/images/basic_16x16_tiles/cities/make_tileset.sh
+++ data/images/basic_16x16_tiles/cities/make_tileset.sh
@@ -7,4 +7,11 @@ convert \( bridge_horiz.png door.png grass.png grass_e.png grass_n.png +append \
 \( path_s.png path_se.png path_sw.png path_w.png path_we.png +append \) \
 \( roof1.png roof2.png roof3.png sand.png tree1.png +append \) \
 \( tree2.png wall.png water.png window.png path_n.png +append \) \
+\( city_wall_sw.png city_wall_se.png city_wall_e.png city_wall_n.png city_wall_w.png +append \) \
+\( pavement.png city_wall_ne.png city_wall_nw.png palace1.png palace2.png +append \) \
+\( palace3.png palace4.png palace5.png palace6.png palace7.png +append \) \
+\( palace8.png palace9.png palace10.png palace11.png palace12.png +append \) \
+\( palace13.png palace14.png palace15.png palace16.png palace17.png +append \) \
+\( palace18.png palace19.png palace20.png palace21.png bridge_vert.png +append \) \
+\( city_wall_sw2.png city_wall_se2.png +append \) \
 -background none -append   tileset_cities.png
--- data/maps/cities_tileset.tsx
+++ data/maps/cities_tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="cities" tilewidth="16" tileheight="16" tilecount="35" columns="5">
- <image source="../images/basic_16x16_tiles/cities/tileset_cities.png" width="80" height="112"/>
+<tileset name="cities" tilewidth="16" tileheight="16" tilecount="70" columns="5">
+ <image source="../images/basic_16x16_tiles/cities/tileset_cities.png" width="80" height="224"/>
  <tile id="11">
   <properties>
    <property name="wall" value="true"/>
@@ -57,6 +57,136 @@
   </properties>
  </tile>
  <tile id="33">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="66">
   <properties>
    <property name="wall" value="true"/>
   </properties>
