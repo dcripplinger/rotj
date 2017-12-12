ca9e736a5380792356dbd37bf2344ef5eacc0f00
David Ripplinger <dcripplinger@gmail.com>
2017-12-12 16:22:50 -0700

    made inn map

--- data/images/basic_16x16_tiles/rooms/make_tileset.sh
+++ data/images/basic_16x16_tiles/rooms/make_tileset.sh
@@ -3,5 +3,6 @@
 convert \( black.png chair.png corner_shadow.png floor.png outer_corner_shadow.png +append \) \
 \( side_shadow.png table.png top_shadow.png under_corner_shadow.png wall.png +append \) \
 \( wall_top.png pot_gray.png floor_green.png corner_shadow_green.png outer_corner_shadow_green.png +append \) \
-\( side_shadow_green.png top_shadow_green.png under_corner_shadow_green.png +append \) \
+\( side_shadow_green.png top_shadow_green.png under_corner_shadow_green.png wall2.png wall_top2.png +append \) \
+\( bed.png bed2.png desk.png bookshelf_left.png bookshelf_right.png +append \) \
 -background none -append tileset_rooms.png
