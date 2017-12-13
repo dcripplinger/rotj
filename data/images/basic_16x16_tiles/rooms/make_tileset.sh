cfa62f883d6338ff37f84fe79eee0e1b9327df04
David Ripplinger <dcripplinger@gmail.com>
2017-12-12 17:04:59 -0700

    added food_shop and home maps

--- data/images/basic_16x16_tiles/rooms/make_tileset.sh
+++ data/images/basic_16x16_tiles/rooms/make_tileset.sh
@@ -5,4 +5,6 @@ convert \( black.png chair.png corner_shadow.png floor.png outer_corner_shadow.p
 \( wall_top.png pot_gray.png floor_green.png corner_shadow_green.png outer_corner_shadow_green.png +append \) \
 \( side_shadow_green.png top_shadow_green.png under_corner_shadow_green.png wall2.png wall_top2.png +append \) \
 \( bed.png bed2.png desk.png bookshelf_left.png bookshelf_right.png +append \) \
+\( floor_red.png outer_corner_shadow_red.png side_shadow_red.png top_shadow_red.png under_corner_shadow_red.png +append \) \
+\( corner_shadow_red.png pot_pic.png +append \) \
 -background none -append tileset_rooms.png
