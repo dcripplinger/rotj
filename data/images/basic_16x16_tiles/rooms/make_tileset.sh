1b44e1c8c2fdc068422fae4fa5ac2affaa86bcb7
David Ripplinger <dcripplinger@gmail.com>
2017-12-12 18:00:45 -0700

    added maps armory, home2, and merchant_shop

--- data/images/basic_16x16_tiles/rooms/make_tileset.sh
+++ data/images/basic_16x16_tiles/rooms/make_tileset.sh
@@ -8,5 +8,6 @@ convert \( black.png chair.png corner_shadow.png floor.png outer_corner_shadow.p
 \( floor_red.png outer_corner_shadow_red.png side_shadow_red.png top_shadow_red.png under_corner_shadow_red.png +append \) \
 \( corner_shadow_red.png pot_pic.png table_left.png table_middle.png table_right.png +append \) \
 \( floor_orange.png outer_corner_shadow_orange.png side_shadow_orange.png top_shadow_orange.png under_corner_shadow_orange.png +append \) \
-\( corner_shadow_orange.png chair_orange.png wall3.png wall_top3.png +append \) \
+\( corner_shadow_orange.png chair_orange.png wall3.png wall_top3.png merchant_sign.png +append \) \
+\( alt_table_left.png alt_table_middle.png alt_table_right.png chair_green.png armory_sign.png +append \) \
 -background none -append tileset_rooms.png
