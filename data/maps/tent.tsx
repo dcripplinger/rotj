d8f1cabca7061e96583845f6ff5564c356882e16
David Ripplinger <dcripplinger@gmail.com>
2018-01-24 08:03:09 -0700

    added tent tileset

---
+++ data/maps/tent.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tent" tilewidth="16" tileheight="16" tilecount="25" columns="5">
+ <image source="../images/basic_16x16_tiles/tents/tileset_tent.png" width="80" height="80"/>
+ <tile id="2">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+</tileset>
