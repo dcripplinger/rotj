cd17c1a2482597b50dd989b09793733678394f42
Justin Moon <jmoon@mooneroid.com>
2018-03-21 10:16:04 -0600

    Updated Lemuel city, made overworld tileset

---
+++ data/maps/overworld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="overworld" tilewidth="16" tileheight="16" tilecount="40" columns="8">
+ <image source="../images/basic_16x16_tiles/overworld/0_tileset_overworld.png" width="128" height="80"/>
+</tileset>
