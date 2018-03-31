1d9db33a89acf0d20e23ce941a99f04ed9758a6d
David Ripplinger <dcripplinger@gmail.com>
2018-03-31 12:48:25 -0600

    fixed desert tile to be a wall

--- data/maps/cities_tileset.tsx
+++ data/maps/cities_tileset.tsx
@@ -1,6 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="cities" tilewidth="16" tileheight="16" tilecount="70" columns="5">
  <image source="../images/basic_16x16_tiles/cities/tileset_cities.png" width="80" height="224"/>
+ <tile id="0">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
  <tile id="11">
   <properties>
    <property name="wall" value="true"/>
