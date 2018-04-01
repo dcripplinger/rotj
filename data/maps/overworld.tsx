abea7b79c3fc6b42ba2f0cd71965a414187b7f90
David Ripplinger <dcripplinger@gmail.com>
2018-03-31 19:30:32 -0600

    added jershon, still have a little more additions to make to it

--- data/maps/overworld.tsx
+++ data/maps/overworld.tsx
@@ -3,184 +3,189 @@
  <image source="../images/basic_16x16_tiles/overworld/0_tileset_overworld.png" width="128" height="80"/>
  <tile id="0">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
  <tile id="1">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
  <tile id="2">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
  <tile id="3">
   <properties>
-   <property name="wall" value="true" />
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="4">
   <properties>
-   <property name="wall" value="true" />
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="5">
   <properties>
-   <property name="wall" value="true" />
-   <property name="water" value="true" />
+   <property name="wall" value="true"/>
+   <property name="water" value="true"/>
   </properties>
  </tile>
  <tile id="6">
   <properties>
-   <property name="wall" value="true" />
-   <property name="water" value="true" />
+   <property name="wall" value="true"/>
+   <property name="water" value="true"/>
   </properties>
  </tile>
  <tile id="7">
   <properties>
-   <property name="wall" value="true" />
-   <property name="water" value="true" />
+   <property name="wall" value="true"/>
+   <property name="water" value="true"/>
   </properties>
  </tile>
  <tile id="8">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
  <tile id="9">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
  <tile id="10">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
  <tile id="11">
   <properties>
-   <property name="wall" value="true" />
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="12">
   <properties>
-   <property name="wall" value="true" />
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="13">
   <properties>
-   <property name="wall" value="true" />
-   <property name="water" value="true" />
+   <property name="wall" value="true"/>
+   <property name="water" value="true"/>
   </properties>
  </tile>
  <tile id="14">
   <properties>
-   <property name="wall" value="true" />
-   <property name="water" value="true" />
+   <property name="wall" value="true"/>
+   <property name="water" value="true"/>
   </properties>
  </tile>
  <tile id="15">
   <properties>
-   <property name="wall" value="true" />
-   <property name="water" value="true" />
+   <property name="wall" value="true"/>
+   <property name="water" value="true"/>
   </properties>
  </tile>
  <tile id="16">
   <properties>
-   <property name="wall" value="true" />
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="17">
   <properties>
-   <property name="wall" value="true" />
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="18">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
  <tile id="19">
   <properties>
-   <property name="wall" value="true" />
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="20">
   <properties>
-   <property name="wall" value="true" />
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="21">
   <properties>
-   <property name="wall" value="true" />
-   <property name="water" value="true" />
+   <property name="wall" value="true"/>
+   <property name="water" value="true"/>
   </properties>
  </tile>
  <tile id="22">
   <properties>
-   <property name="wall" value="true" />
-   <property name="water" value="true" />
+   <property name="wall" value="true"/>
+   <property name="water" value="true"/>
   </properties>
  </tile>
  <tile id="23">
   <properties>
-   <property name="wall" value="true" />
-   <property name="water" value="true" />
+   <property name="wall" value="true"/>
+   <property name="water" value="true"/>
   </properties>
  </tile>
  <tile id="25">
   <properties>
-   <property name="wall" value="true" />
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="26">
   <properties>
-   <property name="wall" value="true" />
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="31">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="34">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
  <tile id="35">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
  <tile id="36">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
  <tile id="38">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
  <tile id="39">
   <properties>
-   <property name="wall" value="false" />
-   <property name="encounter" type="float" value="0" />
+   <property name="encounter" type="float" value="0"/>
+   <property name="wall" value="false"/>
   </properties>
  </tile>
 </tileset>
