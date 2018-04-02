c8f4484be4ea28efcc9b00f3a392431e4ca9c105
David Ripplinger <dcripplinger@gmail.com>
2018-04-02 05:45:30 -0600

    fixed bug with new broken fence tile (not sure how, but Tiled Map Editor finally did the right thing)

--- data/maps/overworld.tsx
+++ data/maps/overworld.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="overworld" tilewidth="16" tileheight="16" tilecount="40" columns="8">
- <image source="../images/basic_16x16_tiles/overworld/0_tileset_overworld.png" width="128" height="80"/>
+<tileset name="overworld" tilewidth="16" tileheight="16" tilecount="48" columns="8">
+ <image source="../images/basic_16x16_tiles/overworld/0_tileset_overworld.png" width="128" height="96"/>
  <tile id="0">
   <properties>
    <property name="encounter" type="float" value="0"/>
