41c2fd78f3f756d5b263122678b85d570f12d039
David Ripplinger <dcripplinger@gmail.com>
2018-07-10 09:24:28 -0600

    updated nephihah's dialog, window placement, stats for warlords in antionum battle

--- src/rotj.py
+++ src/rotj.py
@@ -7,12 +7,12 @@ import pygame
 
 from game import Game
 
-os.environ['SDL_VIDEO_WINDOW_POS'] = "0,0"
+os.environ['SDL_VIDEO_WINDOW_POS'] = "100,100"
 
 if __name__ == '__main__':
     pygame.display.init()
     info_object = pygame.display.Info()
-    screen = pygame.display.set_mode((int(.85*info_object.current_w), int(.85*info_object.current_h)))
+    screen = pygame.display.set_mode((int(.8*info_object.current_w), int(.8*info_object.current_h)))
     pygame.mixer.init(frequency=44100)
     try:
         game = Game(screen)
