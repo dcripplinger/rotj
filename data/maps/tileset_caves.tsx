907dc86ea0ebb80a66601165f5b1c6bb5562a662
David Ripplinger <dcripplinger@gmail.com>
2018-08-13 13:00:09 -0600

    added maps of all the desert caves

--- data/maps/tileset_caves.tsx
+++ data/maps/tileset_caves.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="cave" tilewidth="16" tileheight="16" tilecount="48" columns="8">
- <image source="tileset_caves.png" width="128" height="96"/>
+<tileset name="cave" tilewidth="16" tileheight="16" tilecount="56" columns="8">
+ <image source="../images/basic_16x16_tiles/caves/tileset_caves.png" width="128" height="112"/>
  <tile id="0">
   <properties>
    <property name="encounter" type="float" value="0"/>
@@ -9,6 +9,96 @@
  <tile id="1">
   <properties>
    <property name="encounter" type="float" value="0"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="wall" value="true"/>
   </properties>
  </tile>
  <tile id="28">
@@ -64,6 +154,16 @@
  <tile id="43">
   <properties>
    <property name="encounter" type="float" value="0"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="wall" value="true"/>
   </properties>
  </tile>
 </tileset>
