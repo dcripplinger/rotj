2b2811fcaa9b85fa60ecc5dbf6623fd6d62d9dee
David Ripplinger <dcripplinger@gmail.com>
2019-03-02 07:31:17 -0700

    made screen size aware of windows scaling
    Finally figured out what the heck was wrong. When I grab current_w and
    current_h, that's the resolution the monitor is set at. But Windows can
    also scale text and apps and such in the settings, and my game was
    acting like it didn't know about that. This change checks if we are on a
    Windows system and, if so, calls a dll function that makes it aware of
    scaling. Then when I grab current_h and current_w it is modified
    according to the scaling.

--- src/rotj.py
+++ src/rotj.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import ctypes
 import os
 
 import pygame
 
 from game import Game
 
-os.environ['SDL_VIDEO_WINDOW_POS'] = "100,100"
-
 if __name__ == '__main__':
+    if os.name == 'nt':
+        ctypes.windll.user32.SetProcessDPIAware()
     pygame.display.init()
-    screen = pygame.display.set_mode((800, 600))
+    info_object = pygame.display.Info()
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "{},{}".format(int(.1*info_object.current_w), int(.1*info_object.current_h))
+    screen = pygame.display.set_mode((int(.8*info_object.current_w), int(.8*info_object.current_h)))
     pygame.mixer.init(frequency=44100)
     try:
         game = Game(screen)
