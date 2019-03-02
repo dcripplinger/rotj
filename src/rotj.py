ffbcde0f8172545c876a3a09154c984fb0b376b0
David Ripplinger <dcripplinger@gmail.com>
2019-03-01 17:53:51 -0700

    changed display to 800x600 to work on my current computer
    I still can't figure out how to get the display size right across
    multiple computers. This probably is too tiny on other screens.

--- src/rotj.py
+++ src/rotj.py
@@ -11,8 +11,7 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = "100,100"
 
 if __name__ == '__main__':
     pygame.display.init()
-    info_object = pygame.display.Info()
-    screen = pygame.display.set_mode((int(.8*info_object.current_w), int(.8*info_object.current_h)))
+    screen = pygame.display.set_mode((800, 600))
     pygame.mixer.init(frequency=44100)
     try:
         game = Game(screen)
