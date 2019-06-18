7cadc4dced394d29bbe9c4fbb5495a7da2ac1dd0
David Ripplinger <dcripplinger@gmail.com>
2019-06-18 10:10:42 -0600

    added title to pause menu

--- src/pause_menu.py
+++ src/pause_menu.py
@@ -1,15 +1,18 @@
 from pygame.locals import *
 
-from constants import BLACK
+from constants import BLACK, GAME_WIDTH
+from text import TextBox
 
 
 class PauseMenu(object):
     def __init__(self, screen, game):
         self.screen = screen
         self.game = game
+        self.title = TextBox('PAUSE MENU', GAME_WIDTH, 16, adjust='center')
 
     def draw(self):
         self.screen.fill(BLACK)
+        self.screen.blit(self.title.surface, (0, 8))
 
     def update(self, dt):
         pass
