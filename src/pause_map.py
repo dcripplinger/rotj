8f9969b5bf28a0b22665ae4e343031cccd000938
David Ripplinger <dcripplinger@gmail.com>
2019-06-18 17:11:48 -0600

    player can move around the pause map and exit it

--- src/pause_map.py
+++ src/pause_map.py
@@ -5,7 +5,14 @@ from pygame.locals import *
 import pyscroll
 from pytmx.util_pygame import load_pygame
 
+from constants import TILE_SIZE
 from helpers import get_map_filename
+
+XMIN = 7.5
+XMAX = 291.5
+YMIN = 7
+YMAX = 392
+SPEED = 50 # tiles per second when moving
 
 class PauseMap(object):
     def __init__(self, screen, game, position):
@@ -17,17 +24,38 @@ class PauseMap(object):
         self.map_layer = pyscroll.BufferedRenderer(map_data, self.screen.get_size())
         self.map_layer.zoom = 1
         self.group = pyscroll.group.PyscrollGroup(map_layer=self.map_layer)
-        self.position = position # must be a tuple of two integers (x,y). this is world coordinates.
+        self.bound_position(position)
+        self.direction = [0, 0]
+
+    def bound_position(self, position):
+        self.position = [min(XMAX, max(XMIN, position[0])), min(YMAX, max(YMIN, position[1]))]
 
     def get_group_center(self):
-        return [int((coord+0.5)*16) for coord in self.position]
+        return [int((coord + 0.5) * TILE_SIZE) for coord in self.position]
 
     def draw(self):
         self.group.center(self.get_group_center())
         self.group.draw(self.screen)
 
     def update(self, dt):
-        pass
+        self.position[0] += self.direction[0] * SPEED * dt
+        self.position[1] += self.direction[1] * SPEED * dt
+        self.bound_position(self.position)
+        self.direction = [0, 0] # Keep the map from moving forever
 
     def handle_input(self, pressed):
-        pass
+        if pressed[K_UP]:
+            self.direction = [0, -1]
+        elif pressed[K_DOWN]:
+            self.direction = [0, 1]
+        elif pressed[K_LEFT]:
+            self.direction = [-1, 0]
+        elif pressed[K_RIGHT]:
+            self.direction = [1, 0]
+        else:
+            self.direction = [0, 0]
+
+        if pressed[K_z]:
+            self.game.close_pause_map()
+        elif pressed[K_RETURN]:
+            self.game.close_pause_menu()
