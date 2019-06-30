ffed54858e3dfd9ceeedc3c935020a5d039973b7
David Ripplinger <dcripplinger@gmail.com>
2019-06-30 07:42:29 -0600

    remove robbers from map after battle72

--- src/tiled_map.py
+++ src/tiled_map.py
@@ -251,6 +251,14 @@ class Map(object):
                 key: sprite for key, sprite in self.ai_sprites.items() if sprite.name != 'gidgiddoni'
             }
             self.ai_sprites = ai_sprites
+        elif condition == 'battle72':
+            for sprite in self.group.sprites():
+                if sprite.name == 'robbers':
+                    self.group.remove(sprite)
+            ai_sprites = {
+                key: sprite for key, sprite in self.ai_sprites.items() if sprite.name != 'robbers'
+            }
+            self.ai_sprites = ai_sprites
 
     def try_toggle_equip_on_item(self, user, item_index):
         self.game.try_toggle_equip_on_item(user, item_index)
